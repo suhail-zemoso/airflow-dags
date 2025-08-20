4af0e4f1627275126fd995cd4d9297d094529180
suhail gour <suhail.gour@zemosolabs.com>
2025-08-20 15:41:41 +0530

    git bundle test

---
+++ dags/git_bundle_test.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    "git_bundle_test",
+    start_date=datetime(2025, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    t1 = BashOperator(
+        task_id="whoami_task",
+        bash_command="whoami",
+        run_as_user="run_as_user",
+    )
