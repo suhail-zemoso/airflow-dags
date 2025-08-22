7b5c83131e3ddffdbf05ffb8a0456e790debcc27
suhail gour <suhail.gour@zemosolabs.com>
2025-08-23 00:00:25 +0530

    changes

--- dags/git_bundle_test.py
+++ dags/git_bundle_test.py
@@ -1,17 +1,6 @@
-import sys
-import os
-
-# Add plugins directory to Python path
-plugins_dir = os.path.expanduser("~/airflow/plugins")
-if plugins_dir not in sys.path:
-    sys.path.append(plugins_dir)
-
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
-
-# Import after adding to path
-from custom_bundles.git_bundle import UserSpecificGitDagBundle
 
 with DAG(
     "git_bundle_test",
@@ -19,6 +8,7 @@ with DAG(
     schedule=None,
     catchup=False,
 ) as dag:
+
     t1 = BashOperator(
         task_id="whoami_task",
         bash_command="whoami",
